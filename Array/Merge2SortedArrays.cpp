5d56700bdc3ba26d831f0f12751514f0c20ab023
Tushar Baviskar <tusharkalal177@gmail.com>
2026-02-02 16:38:13 +0530

    add MergeTwoSorted Array problem

--- Array/Merge2SortedArrays.cpp
+++ Array/Merge2SortedArrays.cpp
@@ -1,10 +1,16 @@
-class Solution {
-public:
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// for understating here we have two arrays need to merge them 
+// nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// idx = m+n-1 will be 3+3-1
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         m = nums1.size()-n ; n = nums2.size();
-        int idx=m+n-1, i = m-1, j=n-1;
+        int idx=m+n-1, i = m-1, j=n-1;             
         while(j>=0 && i>=0){
-            if(nums1[i]<=nums2[j]){
+            if(nums1[i]<=nums2[j]){     
                 nums1[idx]=nums2[j]; 
                 j--; 
                 idx--;
@@ -19,4 +25,7 @@ public:
             j--; idx--;
         }
     }
-};
+
+int main(){
+    return 0;
+}
