d525e467a5694cb3457e9e9d2c7c9e30b8fb2174
Tushar Baviskar <tusharkalal177@gmail.com>
2026-02-10 19:33:51 +0530

    add updated solution Find First and Last Position of Element in Sorted Array

--- Array/SortedArray.cpp
+++ Array/SortedArray.cpp
@@ -1,4 +1,16 @@
 //Find First and Last Position of Element in Sorted Array
+// Example 1:
+
+// Input: nums = [5,7,7,8,8,10], target = 8
+// Output: [3,4]
+// Example 2:
+
+// Input: nums = [5,7,7,8,8,10], target = 6
+// Output: [-1,-1]
+// Example 3:
+
+// Input: nums = [], target = 0
+// Output: [-1,-1]
 
 #include<iostream>
 #include<vector>
