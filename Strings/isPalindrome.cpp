b758d30d86f1bd8b0efefed1a7613d01bba93aa0
Tushar Baviskar <tusharkalal177@gmail.com>
2026-02-13 23:06:25 +0530

    add palindrome

---
+++ Strings/isPalindrome.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+string revstring(string s){
+    reverse(s.begin(), s.end());
+
+    return s;
+}
+
+bool isAlphaNumeric(char ch){
+    if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')) return true;
+    else return false;
+}
+
+bool isPalindrome(string s){
+    int st=0 , end=s.length()-1;
+
+    while(st<end){
+
+        if(!isAlphaNumeric(s[st])) {
+            st++; continue;
+        }
+        if(!isAlphaNumeric(s[end])){
+            end--; continue;
+        }
+        if (tolower(s[st]) != tolower(s[end])){
+            return false;
+        }
+        st++; end--;
+    }
+    return true;
+}
+
+int main(){
+
+    string s = "A man, a plan, a canal: Panama";
+
+    string rev = revstring(s);
+
+    cout<<"original: "<<s<<endl;
+    cout<<"reversed: "<<rev<<endl;
+
+    if(isPalindrome(s)){
+        cout<<"it is Plalindrome"<<endl;
+    } else{ 
+        cout<<"it is not a palindrome"<<endl;
+    }
+
+    return 0;
+}
