64067d0ed7ec55e928621c3fba6f5146b115904e
Tushar Baviskar <tusharkalal177@gmail.com>
2026-03-02 20:43:18 +0530

    solution for Reverse words in string

--- Strings/Reverse_words.cpp
+++ Strings/Reverse_words.cpp
@@ -1,3 +1,12 @@
+// Given an input string s, reverse the order of the words.
+
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -33,3 +42,6 @@ int main(){
 
     return 0;
 }
+
+/* approach : reverse whole string then add string of endividual word without spaces through a condn then reverse the word string 
+and add it to ans string with starting from a space and return ans string with STL ans.substr(1) (it will start string form 1 index) */
