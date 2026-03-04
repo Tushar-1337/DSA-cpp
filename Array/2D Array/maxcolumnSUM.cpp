1d9d3fb17e2e8349761d0af2611d6224037e07f9
Tushar Baviskar <tusharkalal177@gmail.com>
2026-03-04 23:58:15 +0530

    MAX column Sum in 2D array

---
+++ Array/2D Array/maxcolumnSUM.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int MaxColumnSum( int mat[][3], int rows, int cols){
+    int maxCount=INT_MIN;      // used INT_MIN coz if matrix has -ve nos. it will still points to +ve as maximum
+    for(int j=0; j<cols; j++){
+        int count = 0;
+
+        for(int i=0; i<rows; i++){
+            count = mat[i][j] + count;
+        }
+        maxCount = max(count,maxCount);
+    }
+    return maxCount;
+}
+
+int main(){
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int rows = 3, cols= 3;
+
+    cout<<MaxColumnSum(matrix,rows,cols);
+
+
+    return 0;
+}
